2a01b96bf674e6acc20fb96099e3d5588c1e9740
salahyaqoob <salah.yaqoob@utb.ecutbildning.se>
2022-04-05 00:53:08 +0200

    add all files

---
+++ src/ts/main.ts
@@ -0,0 +1,172 @@
+
+import { Box } from "./models/content";
+
+let input = document.getElementById("insertTask") as HTMLInputElement;
+let form = document.getElementById("addTask") as HTMLFormElement;
+let tasksDiv = document.getElementById("newTasks") as HTMLDivElement;
+let readytask = document.querySelector("newAddTasks") as HTMLDivElement;
+let btn = document.getElementById("save") as HTMLButtonElement;
+let textErorr = document.getElementById("error") as HTMLSpanElement;
+
+
+//Array&list&localstorage
+//********************************************************************************* */
+
+let listOfTasks: Box[] = [];
+
+if (localStorage.getItem("newTasks")) {
+  listOfTasks = JSON.parse(localStorage.getItem("newTasks"));
+}
+
+btn.addEventListener("click", () => {
+  if (input.value !== null) {
+    addTaskToList(input.value);
+    input.value = "";
+  }
+});
+function addTaskToList(x: string) {
+  let task = new Box(
+    Date.now().toString(),
+    x,
+    false,
+  );
+  listOfTasks.push(task);
+
+  // console.log(task.id);
+
+  addElementsToPageFrom(listOfTasks);
+
+  addElementsToLocalStorage(listOfTasks);
+
+  // console.log(listOfTasks);
+  // console.log(JSON.stringify(listOfTasks));
+
+}
+//********************************************************************************* */
+
+
+
+
+//make tasks
+//********************************************************************************** */
+function addElementsToPageFrom(listOfTasks: Box[]) {
+  tasksDiv.innerHTML = " ";
+  listOfTasks.forEach((t) => {
+
+    let newDiv = document.createElement("div");
+    newDiv.className = "newAddTask";
+
+    if (t.completed) {
+      newDiv.className = "newAddTask done";
+    }
+    newDiv.setAttribute("data-id", t.id);
+    newDiv.appendChild(document.createTextNode(t.title));
+
+    let delSpan = document.createElement("span");
+    delSpan.className = "del";
+    delSpan.appendChild(document.createTextNode("Delete"));//write text(delete) in span
+    newDiv.appendChild(delSpan);
+    tasksDiv.appendChild(newDiv);
+
+  });
+
+}
+console.log(listOfTasks);
+
+
+//********************************************************************************** */
+
+
+
+
+
+//LocalStorage
+//********************************************************************************** */
+getDataFromLocalStorage();
+
+function addElementsToLocalStorage(listOfTasks: Box[]) {
+  localStorage.setItem("newTasks", JSON.stringify(listOfTasks));
+}
+
+function getDataFromLocalStorage() {
+  let data = localStorage.getItem("newTasks");
+  if (data) {
+    let saveData = JSON.parse(data);
+    addElementsToPageFrom(saveData);
+  }
+}
+//********************************************************************************** */
+
+
+
+
+//Delete button
+//********************************************************************************** */
+tasksDiv.addEventListener("click", (e: Event) => {
+  let item = e.target as HTMLSpanElement
+  if (item.classList.contains("del")) {
+    item.parentElement.remove();//from dom
+
+    deleteTaskFromLocalStorage(item.parentElement.getAttribute("data-id"));
+    console.log(item.parentElement.getAttribute("data-id"));
+  }
+
+  // ready task
+  else if (item.classList.contains("newAddTask")) {
+    item.classList.toggle("done");
+
+    toggleReadytTask(item.getAttribute("data-id"));
+  }
+
+
+});
+
+function deleteTaskFromLocalStorage(taskId: string) {
+  // listOfTasks.forEach((t)=> {
+  //   console.log(t.id+'='+taskId)
+  // });
+  listOfTasks = listOfTasks.filter((t) => t.id != taskId);
+  addElementsToLocalStorage(listOfTasks);
+}
+
+//********************************************************************************** */
+
+
+//Ready button
+//********************************************************************************** */
+
+function toggleReadytTask(taskId: string) {
+  for (let i = 0; i < listOfTasks.length; i++) {
+    let status = listOfTasks[i];
+
+    if (status.id != taskId) {
+      status.completed == false ? (status.completed = true) : (status.completed = false);
+
+    }
+  }
+  addElementsToLocalStorage(listOfTasks);
+}
+
+
+//********************************************************************************** */
+
+//text warning
+//********************************************************************************** */
+input.addEventListener("input", () => {
+  checkLength();
+});
+
+function checkLength() {
+  let maxLength = input.value.length;
+  let length: number = 60;
+  if (maxLength == length) {
+    textErorr.hidden = false;
+    input.classList.add("warning")
+
+  } else {
+    textErorr.hidden = true;
+    input.classList.remove("warning")
+  }
+};
+//********************************************************************************** */
+
---
+++ src/ts/models/content.ts
@@ -0,0 +1,8 @@
+export class Box {
+    constructor(
+       public id:string,
+       public title:string,
+       public completed:boolean,
+    ) 
+    {};
+}
